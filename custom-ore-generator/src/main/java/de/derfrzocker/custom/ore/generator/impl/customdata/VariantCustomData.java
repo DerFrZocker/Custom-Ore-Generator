23cd1d1f7c9dbbae17d19ed80674cd684086b703
DerFrZocker <derrieple@gmail.com>
2019-12-21 11:17:53 +0100

    Begin with Variant Customdata
    Took 42 minutes

---
+++ custom-ore-generator/src/main/java/de/derfrzocker/custom/ore/generator/impl/customdata/VariantCustomData.java
@@ -0,0 +1,113 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Marvin (DerFrZocker)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package de.derfrzocker.custom.ore.generator.impl.customdata;
+
+import de.derfrzocker.custom.ore.generator.api.CustomData;
+import de.derfrzocker.custom.ore.generator.api.CustomDataApplier;
+import de.derfrzocker.custom.ore.generator.api.CustomDataType;
+import de.derfrzocker.custom.ore.generator.api.OreConfig;
+import de.derfrzocker.custom.ore.generator.impl.v1_10_R1.customdata.VariantApplier_v1_10_R1;
+import de.derfrzocker.custom.ore.generator.impl.v1_11_R1.customdata.VariantApplier_v1_11_R1;
+import de.derfrzocker.custom.ore.generator.impl.v1_12_R1.customdata.VariantApplier_v1_12_R1;
+import de.derfrzocker.custom.ore.generator.impl.v1_8_R1.customdata.VariantApplier_v1_18_R1;
+import de.derfrzocker.custom.ore.generator.impl.v1_8_R2.customdata.VariantApplier_v1_18_R2;
+import de.derfrzocker.custom.ore.generator.impl.v1_8_R3.customdata.VariantApplier_v1_18_R3;
+import de.derfrzocker.custom.ore.generator.impl.v1_9_R1.customdata.VariantApplier_v1_19_R1;
+import de.derfrzocker.custom.ore.generator.impl.v_1_9_R2.customdata.VariantApplier_v1_19_R2;
+import de.derfrzocker.spigot.utils.Version;
+import org.bukkit.Bukkit;
+import org.bukkit.material.TexturedMaterial;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class VariantCustomData implements CustomData {
+
+    public static final VariantCustomData INSTANCE = new VariantCustomData();
+
+    @Nullable
+    private CustomDataApplier customDataApplier;
+
+    private VariantCustomData() {
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "VARIANT";
+    }
+
+    @NotNull
+    @Override
+    public CustomDataType getCustomDataType() {
+        return CustomDataType.INTEGER;
+    }
+
+    @Override
+    public boolean canApply(@NotNull final OreConfig oreConfig) {
+        return Bukkit.createBlockData(oreConfig.getMaterial()) instanceof TexturedMaterial;
+    }
+
+    @Override
+    public boolean isValidCustomData(@NotNull final Object customData, @NotNull final OreConfig oreConfig) {
+        if (!(customData instanceof Integer))
+            return false;
+
+
+    }
+
+    @NotNull
+    @Override
+    public CustomDataApplier getCustomDataApplier() {
+        if (customDataApplier == null)
+            customDataApplier = getCustomDataApplier0();
+
+        return customDataApplier;
+    }
+
+    private CustomDataApplier getCustomDataApplier0() {
+        switch (Version.getCurrent()) {
+            case v1_12_R1:
+                return new VariantApplier_v1_12_R1(this);
+            case v1_11_R1:
+                return new VariantApplier_v1_11_R1(this);
+            case v1_10_R1:
+                return new VariantApplier_v1_10_R1(this);
+            case v1_9_R2:
+                return new VariantApplier_v1_19_R2(this);
+            case v1_9_R1:
+                return new VariantApplier_v1_19_R1(this);
+            case v1_8_R3:
+                return new VariantApplier_v1_18_R3(this);
+            case v1_8_R2:
+                return new VariantApplier_v1_18_R2(this);
+            case v1_8_R1:
+                return new VariantApplier_v1_18_R1(this);
+        }
+
+        throw new UnsupportedOperationException("Version not supported jet!");
+    }
+
+}
---
+++ impl/v1_8_R3/src/main/java/de/derfrzocker/custom/ore/generator/impl/v1_8_R3/customdata/VariantApplier_v1_18_R3.java
@@ -0,0 +1,77 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Marvin (DerFrZocker)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package de.derfrzocker.custom.ore.generator.impl.v1_8_R3.customdata;
+
+import de.derfrzocker.custom.ore.generator.api.CustomData;
+import de.derfrzocker.custom.ore.generator.api.CustomDataApplier;
+import de.derfrzocker.custom.ore.generator.api.OreConfig;
+import net.minecraft.server.v1_8_R3.BlockPosition;
+import net.minecraft.server.v1_8_R3.IBlockData;
+import net.minecraft.server.v1_8_R3.IBlockState;
+import net.minecraft.server.v1_8_R3.World;
+import org.apache.commons.lang.Validate;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class VariantApplier_v1_18_R3 implements CustomDataApplier {
+
+    @NotNull
+    private final CustomData customData;
+
+    public VariantApplier_v1_18_R3(@NotNull final CustomData data) {
+        Validate.notNull(data, "CustomData can not be null");
+
+        customData = data;
+    }
+
+    @Override
+    public void apply(@NotNull final OreConfig oreConfig, @NotNull final Object location, @NotNull final Object blockAccess) {
+        final BlockPosition blockPosition = (BlockPosition) location;
+        final World world = (World) blockAccess;
+
+        final IBlockData oldIBlockData = world.getType(blockPosition);
+
+        final Optional<Object> objectOptional = oreConfig.getCustomData(customData);
+
+        if (!objectOptional.isPresent())
+            return; //TODO maybe throw exception?
+
+        final IBlockData newIBlockData = oldIBlockData.getBlock().fromLegacyData((Integer) objectOptional.get());
+
+        IBlockState variant = null;
+
+        for (final IBlockState<?> iBlockState : oldIBlockData.getBlock().P().d()) {
+            if (iBlockState.a().equals("variant")) {
+                variant = iBlockState;
+                break;
+            }
+        }
+
+        world.setTypeAndData(blockPosition, oldIBlockData.set(variant, newIBlockData.get(variant)), 2);
+    }
+
+}
